1a578edc15941977e1925ff61056532b402cf2bc
igor <igorpeterssontrab@gmail.com>
2021-08-11 19:59:02 -0400

    feat: added Providers

---
+++ src/Providers/index.js
@@ -0,0 +1,14 @@
+import {HabitsProvider} from "./habits/"
+import {GroupsProvider} from "./groups"
+
+const Providers = ({children}) => {
+    return(
+      <HabitsProvider>
+          <GroupsProvider>
+              {children}
+          </GroupsProvider>
+      </HabitsProvider>  
+    )
+}
+
+export default Providers;
