1d19295d7e032a1d96fc1f429503d247d8e27fb2
Ariel Bernardes <ariel_bernardes@hotmail.com>
2021-08-13 14:28:45 -0300

    feat:subscribed groups

--- src/Pages/Groups/index.js
+++ src/Pages/Groups/index.js
@@ -11,10 +11,16 @@ import {
 // import { Link } from "react-router-dom";
 
 import HeaderLogged from "../../components/HeaderLogged";
+import { useContext } from "react";
+import { GroupsContext } from "../../Providers/groups";
+import { ListCardsContainerDesktop, ListCardsContainerMobile } from "../Community/styles";
+
 
 const Groups = () => {
   const viewport = window.innerWidth;
+  const {groups} = useContext(GroupsContext);
 
+  console.log(groups)
   return (
     <>
       <HeaderLogged />
@@ -23,14 +29,24 @@ const Groups = () => {
           <h2>Organize sua vida</h2>
           <h4>Escolha uma opção: </h4>
           <OptionsContainerMobile>
-            {/**Cards transferidos para community */}
+            {groups.map(group =><ListCardsContainerMobile key = {group.id}>
+              <h4>nome:</h4>
+              <div>{group.name}</div>
+              <h4>categoria:</h4>
+              <div>{group.category}</div>
+              </ListCardsContainerMobile> )}
           </OptionsContainerMobile>
         </ContainerMobile>
       ) : (
         <ContainerDescktop>
           <h1>Escolha seu grupo</h1>
           <OptionsContainerDescktop>
-            {/**Cards transferidos para community */}
+            {groups.map(group =><ListCardsContainerDesktop key = {group.id}>
+              <h4>nome:</h4>
+              <div>{group.name}</div>
+              <h4>categoria:</h4>
+              <div>{group.category}</div>
+              </ListCardsContainerDesktop> )}
           </OptionsContainerDescktop>
         </ContainerDescktop>
       )}
--- src/Providers/groups/index.js
+++ src/Providers/groups/index.js
@@ -1,13 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
 import {createContext, useContext, useState } from "react"
 
 export const GroupsContext = createContext();
 
 export const GroupsProvider = ({children}) => {
     
-    const [groups, setGroups] = useState([])
+    const [groups, setGroups] = useState([]);
+
+    const loadGroups = () => {
+        axios.get('https://kabit-api.herokuapp.com/groups/subscriptions/', {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+            },
+        }).then(response=> setGroups(response.data));
+    }
+
+    useEffect ( () => {
+        loadGroups();
+    },[])
 
     return(
-        <GroupsContext.Provider value ={{groups, setGroups}}>
+        <GroupsContext.Provider value ={{groups, loadGroups}}>
             {children}
         </GroupsContext.Provider>
     )
