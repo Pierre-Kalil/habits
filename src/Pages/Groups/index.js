05f82cfc36477c43253634529e1760bab4d51a81
Ariel Bernardes <ariel_bernardes@hotmail.com>
2021-08-17 00:17:19 -0300

    Feat: edit group

--- src/Pages/Groups/index.js
+++ src/Pages/Groups/index.js
@@ -11,7 +11,7 @@ import {
 // import { Link } from "react-router-dom";
 
 import HeaderLogged from "../../components/HeaderLogged";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { GroupsContext } from "../../Providers/groups";
 import {
   ListCardsContainerDesktop,
@@ -22,14 +22,19 @@ import SinalMais from "../../components/SinalMais";
 import AddGroup from "../../components/AddGroup";
 // import { ActivitiesContext } from "../../Providers/actives";
 import AddActivities from "../../components/AddActivities";
+import EditGroup from "../../components/EditGroup";
+import axios from "axios";
+import { UserContext } from "../../Providers/user";
 
 const Groups = () => {
   const viewport = window.innerWidth;
   const { groups } = useContext(GroupsContext);
-  console.log(groups);
+  const {user} = useContext(UserContext);
+  const [edit,setEdit] = useState(false);
   const [register, setRegister] = useState(false);
   const [addActive, setActive] = useState(false);
-
+  const [id,setId] = useState('')
+  
   return (
     <>
       <HeaderLogged />
@@ -38,6 +43,8 @@ const Groups = () => {
         <OptionsContainerDescktop>
           {groups.map((group) => (
             <ListCardsContainerDesktop key={group.id}>
+              {group.creator.id ===  Number(user) && <button onClick = {()=>{ setEdit(true); setId(group.id)}}>Editar</button>}
+              <div>{edit && <EditGroup setEdit = {setEdit} group = {id} />}</div>
               <h4>nome:</h4>
               <div>{group.name}</div>
               <h4>categoria:</h4>
