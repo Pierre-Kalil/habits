ee9bb1395d27157621a0c2631c4d3c1136a54f76
Lucas Ribeiro <lorde.ainz@gmail.com>
2021-08-17 11:06:41 -0300

    Feat: form style from groups and habts

--- src/components/EditGroup/index.js
+++ src/components/EditGroup/index.js
@@ -1,35 +1,39 @@
 import { useContext } from "react";
 import { GroupsContext } from "../../Providers/groups";
-import { Container, ContainerInputs } from "../AddHabits/styles";
+import { ContainerInputs } from "../AddHabits/styles";
+import { Container } from "../AddGroup/styles";
 import Button from "../Button";
 import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup"
-import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
 
-const EditGroup = ({setEdit, group}) => { 
-    const {editGroup} = useContext(GroupsContext)
-    const schema = yup.object().shape({
-        category: yup.string().required('Campo obrigatório')
-    })
+const EditGroup = ({ setEdit, group }) => {
+  const { editGroup } = useContext(GroupsContext);
+  const schema = yup.object().shape({
+    category: yup.string().required("Campo obrigatório"),
+  });
 
-    const {register, handleSubmit, formState: {errors}} = useForm({resolver: yupResolver((schema))})
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
-    const handleEditGroup = (data) => {
-        editGroup(data,group) 
-        setEdit(false)
-    }
+  const handleEditGroup = (data) => {
+    editGroup(data, group);
+    setEdit(false);
+  };
 
-    return (
-        <>
-            <Container onSubmit = {handleSubmit(handleEditGroup)}>
-                <h4>Nova Categoria:</h4>
-                <ContainerInputs>
-                    <input placeholder = 'nova categoria' {...register('category')} />
-                    <Button type = 'submit' >Editar</Button>
-                </ContainerInputs>
-
-            </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Container onSubmit={handleSubmit(handleEditGroup)}>
+        <h4>Nova Categoria:</h4>
+        <ContainerInputs>
+          <input placeholder="nova categoria" {...register("category")} />
+          <Button type="submit">Editar</Button>
+        </ContainerInputs>
+      </Container>
+    </>
+  );
+};
 export default EditGroup;
