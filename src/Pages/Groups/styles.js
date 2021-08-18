1a7ea447244d0d99e67869f69c2d3e13c5719cdb
Lucas Ribeiro <lorde.ainz@gmail.com>
2021-08-18 12:03:17 -0300

    Fix: page without groups desktop

--- src/Pages/Groups/styles.js
+++ src/Pages/Groups/styles.js
@@ -1,4 +1,4 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
 
 export const ContainerMobile = styled.div`
   height: 90vh;
@@ -64,52 +64,53 @@ export const OptionsContainerDesktop = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  
 `;
 
 export const InputContainerGroup = styled.div`
-/* background: var(--background); */
-border-radius: 10px;
-border: 2px solid var(--roxo-secundario);
-color: var(--roxo-secundario);
-padding: 4px;
-width: 100%;
-display: flex;
-/* flex-direction: column; */
-transition: 0.4s;
-background-color: var(--roxo-secundario);
-
-border-bottom: 6px solid var(--roxo-secundario);
-border-right: 6px solid var(--roxo-secundario);
-
-${props => props.isErrored && css`
-border-color: red;
-svg {
-  color: red;
-}
-`}
-
-input {
-  /* background: transparent; */
-  align-items: center;
-  font-size: 16px;
+  /* background: var(--background); */
+  border-radius: 10px;
   border: 2px solid var(--roxo-secundario);
-  border-radius: 8px;
-  padding: 5px 0px 5px 10px;
-  flex: 1;
-  border: 0;
-  color: var(--roxo-terciario);
-  /* padding-left: 8px; */
-  height:32px;
-}
+  color: var(--roxo-secundario);
+  padding: 4px;
+  width: 100%;
+  display: flex;
+  /* flex-direction: column; */
+  transition: 0.4s;
+  background-color: var(--roxo-secundario);
+
+  border-bottom: 6px solid var(--roxo-secundario);
+  border-right: 6px solid var(--roxo-secundario);
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: red;
+      svg {
+        color: red;
+      }
+    `}
+
+  input {
+    /* background: transparent; */
+    align-items: center;
+    font-size: 16px;
+    border: 2px solid var(--roxo-secundario);
+    border-radius: 8px;
+    padding: 5px 0px 5px 10px;
+    flex: 1;
+    border: 0;
+    color: var(--roxo-terciario);
+    /* padding-left: 8px; */
+    height: 32px;
+  }
   input::placeholder {
     color: var(--roxo-secundario);
     font-size: 16px;
   }
 
-svg {
-  margin-top: 4px;
-}
+  svg {
+    margin-top: 4px;
+  }
 `;
 
 export const ListCardsContainerDesktop = styled.li`
@@ -122,7 +123,12 @@ export const ListCardsContainerDesktop = styled.li`
   margin: 5px 0;
   box-shadow: 1px 1px 4px 1px var(--sombra-box);
   color: #fff;
-  background: linear-gradient(177.48deg, #10002B 6.72%, #3C096C 30.84%, #8E97FD 77.98%);
+  background: linear-gradient(
+    177.48deg,
+    #10002b 6.72%,
+    #3c096c 30.84%,
+    #8e97fd 77.98%
+  );
   border-radius: 50px;
   font-size: 20px;
 
@@ -132,5 +138,69 @@ export const ListCardsContainerDesktop = styled.li`
 
   hr {
     width: 80%;
+  }
+`;
+
+export const ContainerNewUser = styled.div`
+  text-align: center;
+  font-family: Calibri;
+  h2 {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-size: 3rem;
+  }
+  img {
+    width: 18vw;
+    height: 18vw;
+  }
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+  .princ {
+    display: flex;
+    justify-content: space-around;
+    width: 80vw;
+    height: 30vw;
+    align-items: center;
+  }
+  .opção {
+    background: rgba(255, 255, 255, 0.2);
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+    width: 25vw;
+    height: 19vw;
+    h4 {
+      margin-top: 5px;
+    }
+  }
+`;
+export const ContainerNewUserMobile = styled.div`
+  text-align: center;
+  font-family: Calibri;
+  h2 {
+  }
+  img {
+    width: 18vw;
+    height: 18vw;
+  }
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+  .princ {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .opção {
+    background: rgba(255, 255, 255, 0.2);
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+
+    h4 {
+    }
   }
 `;
